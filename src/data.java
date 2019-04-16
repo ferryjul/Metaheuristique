78b68fbbd66854d52dabf3c94c42ca4d99944cf7
jujudu121212 <ferry@etud.insa-toulouse.fr>
2019-04-16 09:33:30 +0100

    data class set up

---
+++ src/data.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class data {
+
+int num_evac;
+int safe_node;
+ArrayList<ArrayList<Integer>> evac_paths;
+
+
+public data() {
+	evac_paths = new ArrayList<ArrayList<Integer>>();
+}
+
+public void set_num_evac(int id) {
+
+}
+
+public void set_safe_node(int id) {
+
+}
+
+public void add_evac_path(ArrayList<Integer> aList) {
+
+}
+
+public void add_edge(int origin, int destination, int duedate, float length, int capacity) {
+
+}
+
+}
+
