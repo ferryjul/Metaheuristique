d91d2b091d20b00549004761837762acd7bcb2e1
jujudu121212 <ferry@etud.insa-toulouse.fr>
2019-04-16 10:00:27 +0100

    data representation class implemented

--- src/data.java
+++ src/data.java
@@ -1,30 +1,53 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class data {
 
-int num_evac;
-int safe_node;
-ArrayList<ArrayList<Integer>> evac_paths;
+private int num_evac;
+private int safe_node;
 
+private ArrayList<ArrayList<Integer>> evac_paths;
+private HashMap<Integer, HashMap<Integer, Edge_data>> edges;
+
+class Edge_data {
+	int duedate;
+	float length;
+	int capacity;
+	
+	public Edge_data(int d, float l, int c) {
+		duedate = d;
+		length = l;
+		capacity = c;
+	}
+}
 
 public data() {
+	// init the data structures
 	evac_paths = new ArrayList<ArrayList<Integer>>();
+	edges = new HashMap<Integer, HashMap<Integer, Edge_data>>();
 }
 
 public void set_num_evac(int id) {
-
+	this.num_evac = id;
 }
 
 public void set_safe_node(int id) {
-
+	this.safe_node = id;
 }
 
-public void add_evac_path(ArrayList<Integer> aList) {
-
+public void add_evac_path(ArrayList<Integer> aList) { // note that aList must be created by caller
+	this.evac_paths.add(aList);
 }
 
 public void add_edge(int origin, int destination, int duedate, float length, int capacity) {
-
+	if(edges.containsKey(origin)) {
+		edges.get(origin).put(destination, new Edge_data(duedate, length, capacity));
+	}
+	else {
+		HashMap<Integer,Edge_data> hm = new HashMap<Integer,Edge_data>();
+		hm.put(destination, new Edge_data(duedate, length, capacity));
+		edges.put(origin, hm);
+	}
 }
 
 }
