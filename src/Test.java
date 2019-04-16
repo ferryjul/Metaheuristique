bd03a8558b40a00c0fb6ba3c0d8609666c5b38f7
jujudu121212 <ferry@etud.insa-toulouse.fr>
2019-04-16 10:20:27 +0100

    fixed data representation and created test file

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+public static void main(String[] args) {
+
+/* On crée l'exemple du TD */
+data TD_data = new data();
+}
+
+}
--- src/data.java
+++ src/data.java
@@ -5,8 +5,9 @@ public class data {
 
 private int num_evac;
 private int safe_node;
-
-private ArrayList<ArrayList<Integer>> evac_paths;
+private int nb_edge;
+private int nb_node;
+private ArrayList<Path_data> evac_paths;
 private HashMap<Integer, HashMap<Integer, Edge_data>> edges;
 
 class Edge_data {
@@ -21,10 +22,33 @@ class Edge_data {
 	}
 }
 
+class Path_data {
+	int origin;
+	ArrayList<Integer> following;
+	int nb_following;
+	int max_rate;
+
+	public Path_data(int o, int nb_f, int rate, ArrayList<Integer> fol) {
+		origin = o;
+		nb_following = nb_f;
+		max_rate = rate;
+		following = fol;
+	}
+	
+}
+
 public data() {
 	// init the data structures
-	evac_paths = new ArrayList<ArrayList<Integer>>();
+	evac_paths = new ArrayList<Path_data>();
 	edges = new HashMap<Integer, HashMap<Integer, Edge_data>>();
+}
+
+public void set_nb_edge(int n) {
+	this.nb_edge = n;
+}
+
+public void set_nb_node(int n) {
+	this.nb_node = n;
 }
 
 public void set_num_evac(int id) {
@@ -35,8 +59,8 @@ public void set_safe_node(int id) {
 	this.safe_node = id;
 }
 
-public void add_evac_path(ArrayList<Integer> aList) { // note that aList must be created by caller
-	this.evac_paths.add(aList);
+public void add_evac_path(int origin, int pop, int rate, int k, ArrayList<Integer> follow) { // note that aList must be created by caller
+	this.evac_paths.add(new Path_data(origin, k, rate, follow));
 }
 
 public void add_edge(int origin, int destination, int duedate, float length, int capacity) {
